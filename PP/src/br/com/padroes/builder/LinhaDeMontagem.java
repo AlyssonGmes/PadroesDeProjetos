884f8f01fc500731eafd3f80f9e9da8bae2b7b7b
AlyssonGmes <Alysson.gomes63@gmail.com>
2022-10-11 01:54:53 -0300

    Atividades prototype e builder

---
+++ PP/src/br/com/padroes/builder/LinhaDeMontagem.java
@@ -0,0 +1,25 @@
+package br.com.padroes.builder;
+
+public class LinhaDeMontagem {
+    FogueteBuilder foguete;
+
+    public LinhaDeMontagem(FogueteBuilder espaconave){
+        this.foguete = espaconave;
+    }
+
+    public void montarFoguete(){
+        foguete.buildModelo();
+        foguete.buildAltura();
+        foguete.buildFabricante();
+        foguete.buildPeso();
+        foguete.buildTipoCombustivel();
+        foguete.buildTotalAssentos();
+        foguete.buildCapacidadeTanqueCombustivel();
+        foguete.buildTipoMotor();
+        foguete.buildVelocidadeMaxima();
+    }
+
+    public Foguete getFoguete(){
+        return foguete.getFoguete();
+    }
+}
